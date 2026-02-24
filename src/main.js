cb8375d5600e3c264674098b31940452cfb6b673
AntonioEsposito <aespo011@gmail.com>
2026-02-24 17:59:45 +0100

    feat: add pillars, feature sections, platforms, privacy strip, and footer

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import './style.css'
 import { initNavbar } from './navbar.js'
+import { tunerMockupHTML } from './mockup-tuner.js'
+import { keyFinderMockupHTML } from './mockup-keyfinder.js'
 
 initNavbar()
+
+// Inject iPhone mockups into their placeholder divs
+const tunerEl = document.getElementById('tuner-mockup')
+if (tunerEl) tunerEl.innerHTML = tunerMockupHTML()
+
+const keyfinderEl = document.getElementById('keyfinder-mockup')
+if (keyfinderEl) keyfinderEl.innerHTML = keyFinderMockupHTML()
